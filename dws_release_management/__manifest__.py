a677481735ea34522352a6c400db38d3cc1c86f0
sebastiandutchworld <99655310+sebastiandutchworld@users.noreply.github.com>
2022-12-09 11:06:29 +0100

    commit

---
+++ dws_release_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "dws release management",
+    "summary": "addon to keep track of releases",
+    "version": "15.0.1.0.0",
+    "category": "Project",
+    "author": "Dutchworld IT solutions",
+    "website": "www.dutchworld.nl",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/release_view.xml",
+    'security/ir.model.access.csv',],
+    "installable": True,
+}
---
+++ dws_release_management/models/release_management.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api, tools, _
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+    
+    addon_name = fields.Char(string='Addon Name')
+    release_id = fields.Many2one('dws.release.management', string='Release')
+
+class DwsReleaseManagement(models.Model):
+    _name = 'dws.release.management'
+    _description = 'dws release management'
+    
+    name = fields.Char(string='Release Number', required=True)
+    description = fields.Char(string='Description')
+    responsible = fields.Many2one('res.users', string='Responsible', required=True)
+    
+    deployed = fields.Selection([('no', 'No'), 
+                                ('git', 'On Git'), 
+                                ('build', 'Build on Jenkins'), 
+                                ('test', 'Deployed on test'), 
+                                ('prod', 'Deployed on production')],
+                                 string='Deployed', required=True)
+    
+    release_date = fields.Date(string='Release Date', required=True)
+    release_line = fields.Many2many('project.task', 'release_id', string='Release Line')
+    customer = fields.Many2one('res.partner', string='Customer', required=True)
+    tags = fields.Many2many('project.tags', string='Tags')
+
+
+
+
